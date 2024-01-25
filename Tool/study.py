069d5904e1b2312a7907b57c22d2257d1f13ecc3
Maksym Trykoz <max3koz@gmail.com>
2024-01-25 15:45:47 +0100

    Added tasks

--- Tool/study.py
+++ Tool/study.py
@@ -1,3 +1,5 @@
+import math
+
 """
 Open and read file
 
@@ -7,6 +9,7 @@ with open('study.py', mode='r+') as file:
     file.write('print(data)\n')
 """
 
+"""
 import sys
 print(sys.getdefaultencoding())
 print(ord('a'))
@@ -25,3 +28,24 @@ print(enc_utf16)
 print(len(enc_ascii))
 print(len(enc_utf8))
 print(len(enc_utf16))
+"""
+
+q_ty_caps = int(input("Enter coffee caps q-ty: "))
+
+q_ty_bonus_cap = q_ty_caps//7
+print(q_ty_bonus_cap)
+
+x1 = int(input("Enter x1: "))
+y1 = int(input("Enter y1: "))
+x2 = int(input("Enter x2: "))
+y2 = int(input("Enter y2: "))
+
+dist = round(math.sqrt((x2-x1)**2 + (y2-y1)**2),3)
+print(dist)
+
+chicken = int(input("Enter q-ty of the chicken: "))
+cow = int(input("Enter q-ty of the cow: "))
+pig = int(input("Enter q-ty of the pig: "))
+
+legs = chicken*2 + (cow + pig)*4
+print(legs)
